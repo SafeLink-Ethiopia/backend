8e093050f254db753a6abc3f5e1a0582fad4fbf0
unknown <sifenbeyan@gmail.com>
2026-09-22 12:57:19 +0300

    fix awareness'

--- src/routes/awarenessRoutes.ts
+++ src/routes/awarenessRoutes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { getConsentAwareness } from "../controllers/awarenessController";
+
+import {
+  getConsentAwareness,
+  getBoundariesAwareness,
+  getHarassmentAwareness,
+  getSupportAwareness,
+} from "../controllers/awarenessController";
 
 const router = Router();
 
 router.get("/consent", getConsentAwareness);
+router.get("/boundaries", getBoundariesAwareness);
+router.get("/harassment", getHarassmentAwareness);
+router.get("/support", getSupportAwareness);
 
 export default router;
